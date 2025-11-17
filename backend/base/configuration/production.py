d133a4388987cf8297e2c4fd3b35ff3b6ac4b588
abde1khaliq <ahmedthek3@gmail.com>
2025-11-17 07:54:35 +0200

    switched from mysql to sqlite because im broke.

--- backend/base/configuration/production.py
+++ backend/base/configuration/production.py
@@ -12,17 +12,16 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myproject.settings.development'
 
 from .config import *
 from decouple import config, Csv
-import dj_database_url
 
 SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=True, cast=bool)
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default=config('DATABASE_URL', default=''),
-        conn_max_age=600
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 
